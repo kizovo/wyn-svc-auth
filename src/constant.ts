ede8fb2fd60b20e71986945f2e75bed8cb791a2a
Rheza Sindhuwinata <rsindhuwinata@prixa.ai>
2023-07-07 20:32:41 +0700

    Add error handling

--- src/constant.ts
+++ src/constant.ts
@@ -2,6 +2,10 @@ const constant = {
   headers: {
     'x-powered-by': 'wyn',
   },
+  error_code: {
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500
+  },
 }
 
 export default constant;
--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,7 @@ import constant from '@/constant'
 import requestValidation from '@handlers/RequestValidationHandler'
 import UserHandler from '@handlers/UserHandler'
 import { ISignupReq } from '@models/UserModel'
+import { errorResponse } from '@handlers/BasicHandler'
 
 const userHandler = new UserHandler
 
@@ -21,6 +22,19 @@ const routes = ({
         set.headers = constant.headers,
         userHandler.listUser()
       ))
+  })
+  .onError(({ code, error, set }) => {
+    console.log(error)
+    if (code === 'NOT_FOUND') {
+      const code_num = constant.error_code[code]
+      set.status = code_num
+      return errorResponse(code_num.toString(), `We looked everywhere, but it seems the file you're searching for is on vacation. It left no forwarding address!`)
+    }
+    if (code === 'INTERNAL_SERVER_ERROR') {
+      const code_num = constant.error_code[code]
+      set.status = code_num
+      return errorResponse(code_num.toString(), `Uh-oh! Our server gremlins are up to their mischief again. They've hidden the requested page in a parallel universe. We're on it!`)
+    }
   })
 
 export default routes;
